0e713c7027f23ac736a04325970e2bb1effcc427
Olavo <olavo.77@hotmail.com>
2022-02-09 21:23:36 -0300

    fix: removidos comentários desnecessários

--- csv2json.py
+++ csv2json.py
@@ -1,25 +1,15 @@
-# Dependências
 import csv
 import json
 
 
-# Função que transforma um arquivo CSV em JSON
-# Recebe como parâmetro o endereço do arquivo CSV
 def csv2json(csv_filepath):
-    # Abre o arquivo CSV
     csv_file = open(csv_filepath, 'r', errors="ignore")
-    # Abre um arquivo JSON para escrita
-    # Passe dentro do "open" o endereço onde quer que o arquivo seja salvo
     json_file = open(r'resultado.json', 'w')
 
-    # Instancia um leitor de CSV
-    # CASO SEU ARQUIVO SEJA DELIMITADOR COM ";", MUDE O DELIMITER
+    # caso necessário, mude o DELIMITER para ";"
     reader = csv.DictReader(csv_file, delimiter=",")
-    # Transforma o arquivo em JSON
     archive = json.dumps([row for row in reader], ensure_ascii=False)
-    # Escreve o JSON no arquivo aberto anteriormente
     json_file.write(archive)
 
 
-# Chama a função passando o endereço do arquivo CSV
-csv2json(r'exemplo.csv')
+csv2json(r'obligation_template.csv')
